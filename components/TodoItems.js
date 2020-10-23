9c8087ce5018955f0c89673333672dbeec71d3f3
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-23 11:58:33 -0400

    Create state variables and change required item prop from title to text

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -1,31 +1,43 @@
 import React from 'react'
+import { useState } from 'react'
 import { FlatList, SafeAreaView, View, Text } from 'react-native'
 
 
 const TEST_DATA = [
   {
     id: '0',
-    title: 'First test item'
+    text: 'First test item'
   },
   {
     id: '1',
-    title: 'Second test item'
+    text: 'Second test item'
   },
   {
     id: '2',
-    title: 'Third test item'
+    text: 'Third test item'
   }
 ]
 
-const Item = ({ title }) => ( 
+const Item = ({ text }) => ( 
   <View>
-    <Text>{title}</Text>
+    <Text>{text}</Text>
   </View>
 )
 
 const TodoItems = () => {
+  // todo object format:
+  /* 
+    {
+      id: (0, 1, 2...),
+      text: "text here"
+    }
+  */
+  // function state using hooks
+  const [todos, setTodos] = useState( [{}] ) // list of objects
+
+  // render individual items through FlatList prop 'renderItem'
   const renderItem = ({ item }) => (
-    <Item title={item.title} />
+    <Item text={item.text} />
   )
 
   return (
