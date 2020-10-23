3a3bd72f1757d148850707412454efe3f27a4c9f
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-23 12:18:19 -0400

    Add state and pass it to TodoItems as a prop

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
 import TodoItems from './components/TodoItems'
 
 const App = () => {
+  // todo object format:
+  /* 
+    {
+      id: (0, 1, 2...),
+      text: "text here"
+    }
+  */
+  // function state using hooks
+  const [todos, setTodos] = useState( 
+    [
+      {
+        id: '0', 
+        text: "first test item"
+      },
+      {
+        id: '1',
+        text: "second test item"
+      },
+      {
+        id: '2',
+        text: "third test item"
+      }
+    ] 
+  ) // list of objects
+
   return (
-    <TodoItems />
+    <TodoItems todos={todos}/>
   )
 }
 
