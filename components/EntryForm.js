47de5a4687ed98c610d1643ec24abf101ffec242
DrewDalmedo <andrewdalmedo@gmail.com>
2020-10-26 16:34:15 -0400

    Add basic form for submitting new todo items

---
+++ components/EntryForm.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, SafeAreaView, TextInput, Button } from 'react-native'
+import { Formik } from 'formik'
+
+const EntryForm = ({ setTodos }) => {
+
+  const onSubmit = (values, actions) => {
+    // temporary 
+    console.log(values)
+  }
+
+  return (
+    <Formik
+      initialValues={{ todo: '' }}
+      onSubmit={onSubmit}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <SafeAreaView>
+          <TextInput 
+            onChangeText={handleChange('todo')}
+            onBlur={handleBlur('todo')}
+            value={values.todo}
+          />
+          <Button 
+            onPress={handleSubmit}
+            title="Submit"
+          />
+        </SafeAreaView>
+      )}
+    </Formik>
+
+    // old way
+    /*
+    <Formik
+      initialValues={{
+        todo: ''
+      }}
+      onSubmit={onSubmit}
+      render={formikProps => 
+        {
+          <>
+            <TextInput
+              value={formikProps.values.todo}
+              onChangeText={formikProps.handleChange('name')}
+            />
+            <Button 
+              title="Submit"
+              onPress={() => formikProps.handleSubmit()}
+            />
+          </>
+        }
+      }
+    />
+    */
+
+  );
+}
+
+export default EntryForm;
